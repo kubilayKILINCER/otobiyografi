5784df972d253bc106512e380ff33336dfb7de80
kubilayKILINCER <147497375+kubilayKILINCER@users.noreply.github.com>
2024-04-25 12:16:49 +0300

    Add files via upload

---
+++ search.js
@@ -0,0 +1,7 @@
+const topbar = document.getElementById('searchEngine')
+const searchEngine = document.getElementById('searchEngine')
+
+
+topbar.addEventListener('click', ()=>{
+   searchEngine.classList.toggle('active')
+})
